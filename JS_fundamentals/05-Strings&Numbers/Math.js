0f4049cc2b3df7416fe33f72b74b084c04fe6838
Kamran Khan <kamran.revnix@gmail.com>
2026-07-28 20:23:16 +0000

    math.js added, strings&numbers folder completed

---
+++ JS_fundamentals/05-Strings&Numbers/Math.js
@@ -0,0 +1,29 @@
+// Math
+let num = 25.333;
+console.log(Math.pow(num,5));
+console.log(Math.sqrt(49))
+console.log(Math.ceil(num))//26
+console.log(Math.floor(num))//25
+
+// imp   ---> Math.random
+// gives values between 0-1
+console.log(Math.random())
+
+   // 0-10
+   console.log(Math.random()*10)
+
+   // 1-10
+   console.log(Math.random()*10+1)
+
+   // .982842 to cut this
+
+   console.log(Math.floor(Math.random()*10+1))
+
+   // when max , min is given then
+
+   let min= 20;
+   let max=30;
+
+   console.log(Math.floor(Math.random() * (max-min + 1) + min ))
+
+
