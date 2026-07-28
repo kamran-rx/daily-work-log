2d4483b7eaced53a0a1299cb068ff5c3c47bee22
Kamran Khan <kamran.revnix@gmail.com>
2026-07-28 20:14:13 +0000

    number.js added

---
+++ JS_fundamentals/05-Strings&Numbers/Number.js
@@ -0,0 +1,12 @@
+// Numbers in JS
+let num = 17; // creates a primitive number 
+let num_2 = new Number(17) // creates a Number object (an object wrapper around a number).
+
+
+ // 1.  toFixed()
+ let number = 123.1134
+console.log(number.toFixed(2)) // 123.11 (after decimal how much values)
+// 2. toPrecision()
+console.log(number.toPrecision(3)) // focused on the left before decimal
+
+
