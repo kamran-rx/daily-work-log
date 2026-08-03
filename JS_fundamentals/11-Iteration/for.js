7b2cafc8fb44844ff1ddfe1a0d1863d635fc39fd
Kamran Khan <kamran.revnix@gmail.com>
2026-08-03 18:04:25 +0000

    iteration folder added + updated

---
+++ JS_fundamentals/11-Iteration/for.js
@@ -0,0 +1,38 @@
+// // loop
+// for ( let i = 1 ; i<=10 ; i++){
+             
+//     console.log( `${i} * ${i} = ${i*i} `)
+// }
+
+// // On array
+
+// let arr = ["a","b","c","d","e","f","g","h","i","j"]
+
+// for (let i = 0 ; i < arr.length ; i++ ){
+//        console.log(` array element ${i} : ${arr[i]}`)
+// }
+
+// //  On objects
+
+// let obj = {
+//                name : "ilyas" ,
+//                age :22 ,
+//                school:"xyz"
+// }
+//       let derived_arr =  Object.keys(obj)
+//      // let d_arr = Object.values(obj)
+ 
+// for (let i = 0 ; i < derived_arr.length ; i++){
+          
+//     console.log(derived_arr[i])
+// }
+
+// nested for loop
+
+for(let i = 1 ; i<=10 ; i++){
+      console.log("----------")
+          for(let j = 1 ; j<=10 ; j++){
+                console.log( `${i} * ${j} = ${i*j}`)
+              
+          }
+}
