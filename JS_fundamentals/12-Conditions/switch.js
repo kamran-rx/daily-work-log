e1798d37cc6a6d4fd7d4eee4181180d163aa5dfe
Kamran Khan <kamran.revnix@gmail.com>
2026-08-03 18:34:55 +0000

    switch.js added

---
+++ JS_fundamentals/12-Conditions/switch.js
@@ -0,0 +1,45 @@
+// switch statement
+
+let month  =  8 ;
+
+switch (month) {
+    case 1:
+                  console.log(" january ")
+        break;
+        case 2:
+                  console.log(" febuary ")
+        break;
+        case 3:
+                  console.log(" march ")
+        break;
+        case 4:
+                  console.log(" april ")
+        break;
+        case 5:
+                  console.log(" may ")
+        break;
+        case 6:
+                  console.log(" june ")
+        break;
+        case 7:
+                  console.log(" july ")
+        break;
+        case 8:
+                  console.log(" august ")
+        break;
+        case 9:
+                  console.log(" September ")
+        break;
+        case 10:
+                  console.log(" October ")
+        break;
+        case 11:
+                  console.log(" November ")
+        break;
+        case 12 :
+            console.log ("December")
+
+    default:
+              console.log("not found")
+        break;
+}
